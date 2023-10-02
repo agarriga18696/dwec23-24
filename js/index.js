48a9b8ebc3254e46f4ef89b4a5f45a9284beec17
agarriga18696 <agarriga18696@iesjoanramis.org>
2023-10-02 19:21:37 +0200

    added js stuff

---
+++ js/index.js
@@ -0,0 +1,71 @@
+// typeof nos dice que tipo de variable es
+
+// var, let, const
+var uno = 1;
+let dos = 2;
+const PI = 3.14151692;
+
+// String
+let nombre = "Andreu";
+let apellido = 'Garriga';
+let nombreCompleto = "Andreu Garriga";
+let comillas = "Texto \"sdsds\"";
+
+// Numeros
+let edad = -25;
+let anyos = 25;
+let precio = 21.25;
+let aprobados = Infinity;
+let suspendidos = -Infinity;
+
+// Null
+let valor = null;
+
+// NaN
+let miedad = "af" / 2;
+
+// typeOf, delete
+let persona = {
+    nombre: "Andreu",
+    edad: 25
+}
+
+/*console.log(persona.nombre);
+delete persona.nombre;
+console.log(persona.nombre);*/
+
+// Boolean
+let algo;
+
+/*
+if(algo) {
+    console.log("hola");
+} else {
+    console.log("adios");
+}
+
+*/
+
+// Operadores Aritméticos
+// --> operaciones matematicas
+/*
+
++   sumar
+-   restar
+*   multiplicar
+/   dividir
+%   modulo (resto)
+++  incremento
+--  decremento
++=  suma y asigna
+-=  resta y asigna
+/=  divide y asigna
+*=  multiplica y asigna
+%=  modulo y asigna
+
+- cambio de signo
+
+!=  diferente a
+
+
+*/
