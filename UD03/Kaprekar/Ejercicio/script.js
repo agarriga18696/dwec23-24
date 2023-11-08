b91d8642ec094db75a9a2f8c81b7cb6fc3ef829c
agarriga18696 <agarriga18696@iesjoanramis.org>
2023-11-08 20:25:48 +0100

    exercici joc preguntes

---
+++ UD03/Kaprekar/Ejercicio/script.js
@@ -0,0 +1,98 @@
+//creamos array de objetos
+const questions = [
+    {
+        questio: "Quin pais té més població?",
+        respostaCorrecta: "La Xina",
+        respostaIncorrecta: "La India",
+    },
+    {
+        questio: "El primer astronàuta a trepitjar la lluna va ser?",
+        respostaCorrecta: "Neil Armstrong",
+        respostaIncorrecta: "Louis Armstrong",
+    }
+]; // aqui va ; ya que acaba el array
+//variables necesarias
+//variable  para saber en qué parte del array estamos:
+let indexQuestioActual = 0;
+//contadores de respuestas
+let respostesCorrectes = 0;
+let respostesIncorrectes = 0;
+
+const questioProposada = document.getElementById("questioProposada");
+const btnEsquerre = document.getElementById("btnEsquerre");
+const btnDret = document.getElementById("btnDret");
+const btnReiniciar = document.getElementById("btnReiniciar");
+const missatge = document.getElementById("missatge");
+
+function barrejaRespostes(correcta, incorrecta) { // para poner las preguntas y respuestas en los lados aleatoriamente
+    
+    const respostes = [correcta, incorrecta];
+    respostes.sort(() => Math.random() - 0.5);
+    return respostes;
+
+}
+
+function mostraQuestio() {
+
+    if(indexQuestioActual < questions.length) {
+        const questioActual = questions[indexQuestioActual];
+        questioProposada.textContent = questioActual.questio;
+
+        const [barrejatCorrecte, barrejatIncorrecte] = barrejaRespostes( // vuelca cada elemento en los botones 
+            questioActual.respostaCorrecta,
+            questioActual.respostaIncorrecta
+        );
+
+        btnEsquerre.textContent = barrejatCorrecte;
+        btnDret.textContent = barrejatIncorrecte;
+    } else {
+        //el juego ha terminado
+        if(respostesCorrectes === questions.length) {
+            missatge.textContent = "Has guanyat!! Has respost totes les questions correctament!";
+        } else {
+            missatge.textContent = `Joc acabat. Respostes correctes: ${ respostesCorrectes }, Respostes incorrectes: ${ respostesIncorrectes}`;
+        }
+
+        btnEsquerre.style.display = "none";
+        btnDret.style.display = "none";
+        btnReiniciar.style.display = "block";
+    }
+
+}
+
+function comprovaResposta(respostaSeleccionada) {
+
+    const questioActual = questions[indexQuestioActual];
+
+    if(respostaSeleccionada === questioActual.respostaCorrecta) {
+        respostesCorrectes++;
+    }
+    else {
+        respostesIncorrectes++;
+    }
+
+    indexQuestioActual++;
+
+    mostraQuestio();
+
+}
+
+// addEventListener : estar pendiente de los eventos que tiene que ver con ese botón.
+btnEsquerre.addEventListener("click", () => comprovaResposta(btnEsquerre.textContent));
+btnDret.addEventListener("click", () => comprovaResposta(btnDret.textContent));
+btnReiniciar.addEventListener("click", () => {
+
+    indexQuestioActual = 0;
+    respostesCorrectes = 0;
+    respostesIncorrectes = 0;
+    missatge.textContent = "";
+    btnEsquerre.style.display = "inline-block";
+    btnDret.style.display = "inline-block";
+    btnReiniciar.style.display = "none";
+
+    mostraQuestio();
+
+});
+
+// Començar el joc
+mostraQuestio();
